277b44c0f8d324181c12951258ab0ce43b0b9fdf
linus <gudgeon.christopher@gmail.com>
2021-10-10 18:33:55 +0100

    only show window when iframe is loaded

--- src/components/window/index.tsx
+++ src/components/window/index.tsx
@@ -16,13 +16,16 @@ const Window: FunctionalComponent<Props> = (props: Props) => {
     const [height, setHeight] = useState(props.height)
     const [x, setX] = useState(props.x)
     const [y, setY] = useState(props.y)
+    const [loaded, setLoaded] = useState(0)
+    const myIframe = <iframe src={props.iframeUrl} width="100%" height={height - 24} class={style.windowIframe} onLoad={() => {setLoaded(1); console.log("asdf")}}></iframe>
   return (
+    loaded == 1 ? 
     <div class={style.window} style={{width: width, height: height, left: x, top: y}}>
         <div class={style.windowHeader}>
             <img src="./assets/images/interrogation_mark.png"/>Muh header
         </div>
-        <iframe src={props.iframeUrl} width="100%" height={height - 24} class={style.windowIframe}></iframe>
-    </div>
+        {myIframe}
+    </div> : myIframe
   );
 };
 
